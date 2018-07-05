84fc6c8e2271e41b38ec441c1c497eaeb6f6f2a6
Phil Howard <phil@gadgetoid.com>
2018-07-05 14:38:15 +0000

    Changed to 400k

--- functions/MLX90640_RPI_I2C_Driver.cpp
+++ functions/MLX90640_RPI_I2C_Driver.cpp
@@ -30,7 +30,7 @@ int MLX90640_I2CRead(uint8_t slaveAddr, uint16_t startAddress, uint16_t nMemAddr
     if(!init){
         bcm2835_init();
     	bcm2835_i2c_begin();
-	bcm2835_i2c_set_baudrate(1000000);
+	bcm2835_i2c_set_baudrate(400000);
 	init = 1;
     }
 
