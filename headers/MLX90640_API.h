4d223a954917befa9652e255cd19924b83ff2233
Phil Howard <phil@gadgetoid.com>
2019-11-07 14:21:21 +0000

    Manual sync with upstream

--- headers/MLX90640_API.h
+++ headers/MLX90640_API.h
@@ -16,8 +16,10 @@
  */
 #ifndef _MLX640_API_H_
 #define _MLX640_API_H_
+
+#define SCALEALPHA 0.000001
     
-  typedef struct
+typedef struct
     {
         int16_t kVdd;
         int16_t vdd25;
@@ -32,12 +34,15 @@
         uint8_t resolutionEE;
         uint8_t calibrationModeEE;
         float KsTa;
-        float ksTo[4];
-        int16_t ct[4];
-        float alpha[768];    
+        float ksTo[5];
+        int16_t ct[5];
+        uint16_t alpha[768];    
+        uint8_t alphaScale;
         int16_t offset[768];    
-        float kta[768];    
-        float kv[768];
+        int8_t kta[768];
+        uint8_t ktaScale;    
+        int8_t kv[768];
+        uint8_t kvScale;
         float cpAlpha[2];
         int16_t cpOffset[2];
         float ilChessC[3]; 
@@ -54,8 +59,8 @@
     void MLX90640_CalculateTo(uint16_t *frameData, const paramsMLX90640 *params, float emissivity, float tr, float *result);
     int MLX90640_SetResolution(uint8_t slaveAddr, uint8_t resolution);
     int MLX90640_GetCurResolution(uint8_t slaveAddr);
-    int MLX90640_SetRefreshRate(uint8_t slaveAddr, uint8_t refreshRate);
-    int MLX90640_GetRefreshRate(uint8_t slaveAddr);
+    int MLX90640_SetRefreshRate(uint8_t slaveAddr, uint8_t refreshRate);   
+    int MLX90640_GetRefreshRate(uint8_t slaveAddr);  
     int MLX90640_GetSubPageNumber(uint16_t *frameData);
     int MLX90640_GetCurMode(uint8_t slaveAddr); 
     int MLX90640_SetInterleavedMode(uint8_t slaveAddr);
