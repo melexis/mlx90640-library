d76cda5a57a18595b812569b775e7c9aea22af80
Xiangmin Xu <65858311+Maripoforest@users.noreply.github.com>
2022-03-21 09:32:30 +0000

    Update MLX90640_I2C_Driver.h

--- headers/MLX90640_I2C_Driver.h
+++ headers/MLX90640_I2C_Driver.h
@@ -20,7 +20,6 @@
 #include <stdint.h>
 
     void MLX90640_I2CInit(void);
-    int MLX90640_I2CGeneralReset(void);
     int MLX90640_I2CRead(uint8_t slaveAddr,uint16_t startAddress, uint16_t nMemAddressRead, uint16_t *data);
     int MLX90640_I2CWrite(uint8_t slaveAddr,uint16_t writeAddress, uint16_t data);
     void MLX90640_I2CFreqSet(int freq);
