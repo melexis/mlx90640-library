97a6dc12b37af7abc36be1f0a65a650f863d6109
Xiangmin Xu <65858311+Maripoforest@users.noreply.github.com>
2022-03-28 13:20:06 +0100

    Update mlx90640_driver.cpp

--- mymlx90640/mlx90640_driver.cpp
+++ mymlx90640/mlx90640_driver.cpp
@@ -30,18 +30,6 @@
 #include <thread>
 #define MLX_I2C_ADDR 0x33
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_NONE    "\x1b[30m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
-//#define FMT_STRING "%+06.2f "
-#define FMT_STRING "\u2588\u2588"
-
 // I2C
 void MLX90640::registerCallback(testpic* pic) {
     mlx90640_callback = pic;
