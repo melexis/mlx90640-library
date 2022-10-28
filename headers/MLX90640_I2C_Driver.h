8fe3383e791db813800012bc964b6c500e18cb9e
Slaveyko <38420877+slavysis@users.noreply.github.com>
2022-10-28 16:07:59 +0300

    Remove the mbed references and define names for the internal addresses

--- headers/MLX90640_I2C_Driver.h
+++ headers/MLX90640_I2C_Driver.h
@@ -18,10 +18,11 @@
 #define _MLX90640_I2C_Driver_H_
 
 #include <stdint.h>
+#include "MLX90640_API.h"
 
-    void MLX90640_I2CInit(void);
-    int MLX90640_I2CGeneralReset(void);
-    int MLX90640_I2CRead(uint8_t slaveAddr,uint16_t startAddress, uint16_t nMemAddressRead, uint16_t *data);
-    int MLX90640_I2CWrite(uint8_t slaveAddr,uint16_t writeAddress, uint16_t data);
-    void MLX90640_I2CFreqSet(int freq);
+    extern void MLX90640_I2CInit(void);
+    extern int MLX90640_I2CGeneralReset(void);
+    extern int MLX90640_I2CRead(uint8_t slaveAddr,uint16_t startAddress, uint16_t nMemAddressRead, uint16_t *data);
+    extern int MLX90640_I2CWrite(uint8_t slaveAddr,uint16_t writeAddress, uint16_t data);
+    extern void MLX90640_I2CFreqSet(int freq);
 #endif
